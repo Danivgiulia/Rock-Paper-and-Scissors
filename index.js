6c2a233014a222f374cd9f26054f8fa96c957b77
Danivgiulia <danielavgiulianelli@gmail.com>
2025-02-05 00:42:30 +0000

    pushing to deploy

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Rock, Paper and Scissors game 
 
-// Declaring my constants first
+// Declaring my constants and variables first
 
 const buttons = ["rock", "paper","scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
 // Function for the game
 function playGame(playerChoice){
@@ -19,13 +23,13 @@ function playGame(playerChoice){
      else {
             switch(playerChoice){
                 case "rock":
-                    result = (computerChoice === "scissors") ? "You Win" : "You Lose!";
+                    result = (computerChoice === "scissors") ? "You Win" : "You Lose";
                     break;
                 case "paper":
-                    result = (computerChoice === "rock") ? "You Win" : "You Lose!";
+                    result = (computerChoice === "rock") ? "You Win" : "You Lose";
                     break;
                 case "scissors":
-                    result = (computerChoice === "paper") ? "You Win" : "You Lose!";
+                    result = (computerChoice === "paper") ? "You Win" : "You Lose";
                     break;
             }
     }
@@ -33,5 +37,16 @@ function playGame(playerChoice){
     playerDisplay.textContent = `Your choice: ${playerChoice}`;
     computerDisplay.textContent = `Computer: ${computerChoice}`;
     resultDisplay.textContent = result;
+
+    switch(result){
+        case "You Win":
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "You Lose":
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+    }
 
 }
